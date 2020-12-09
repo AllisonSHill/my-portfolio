dc06aaa09a70835d64439fa542a6f1bc682c5bd7
allisonhill00 <allison.m.s.hill@gmail.com>
2020-12-09 11:45:24 -0700

    Updates

---
+++ precache-manifest.ef9f48ba6f4c00da42b73709084b287c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c42e9c4f4ed2c14862494d1e097089",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "a66e16d61d1272ce5e32",
+    "url": "/my-portfolio/static/css/main.f20f24ef.chunk.css"
+  },
+  {
+    "revision": "31850b0f8dd9bb4b6b39",
+    "url": "/my-portfolio/static/js/2.45f37842.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.45f37842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a66e16d61d1272ce5e32",
+    "url": "/my-portfolio/static/js/main.32e1b074.chunk.js"
+  },
+  {
+    "revision": "71f8fe099066da5003c6",
+    "url": "/my-portfolio/static/js/runtime-main.f6d94e01.js"
+  },
+  {
+    "revision": "f32445be6118539131853bc24dc2d1b1",
+    "url": "/my-portfolio/static/media/CFI.f32445be.jpg"
+  },
+  {
+    "revision": "b5ff1a795240a897a3a71df1d8078e8e",
+    "url": "/my-portfolio/static/media/MoWater.b5ff1a79.jpg"
+  },
+  {
+    "revision": "f8d9a56f52d56e92fdbe7803409a83aa",
+    "url": "/my-portfolio/static/media/accessfund.f8d9a56f.png"
+  },
+  {
+    "revision": "0020a1106be1c7ab74f14a0b90f9e47d",
+    "url": "/my-portfolio/static/media/bennyAndFlo.0020a110.jpg"
+  },
+  {
+    "revision": "5bc48dd7024bfe634481d9bc3c9f08cf",
+    "url": "/my-portfolio/static/media/bg_parallax.5bc48dd7.png"
+  },
+  {
+    "revision": "c458c2b7839d4889da5ebbd3540b7088",
+    "url": "/my-portfolio/static/media/biersAndMo.c458c2b7.jpg"
+  },
+  {
+    "revision": "2c985f617c3c8a0634ba47e04cb88c78",
+    "url": "/my-portfolio/static/media/biersMountain.2c985f61.jpg"
+  },
+  {
+    "revision": "f3d456a80065481762e57054543c8775",
+    "url": "/my-portfolio/static/media/blm.f3d456a8.png"
+  },
+  {
+    "revision": "34ada1638ef7bef370e593e45e2cd29a",
+    "url": "/my-portfolio/static/media/instagram.34ada163.png"
+  },
+  {
+    "revision": "7537d0ec68e8dc50318a018412937608",
+    "url": "/my-portfolio/static/media/lnt.7537d0ec.png"
+  },
+  {
+    "revision": "7bb1ba42cc115e8598207e06a90282f7",
+    "url": "/my-portfolio/static/media/meGlasses.7bb1ba42.jpg"
+  },
+  {
+    "revision": "68f3a73d5b93a3e47d405fa9e6c377ce",
+    "url": "/my-portfolio/static/media/mid_parallax.68f3a73d.png"
+  },
+  {
+    "revision": "06b3af4483b4472110cc7e3be073537c",
+    "url": "/my-portfolio/static/media/top_parallax.06b3af44.png"
+  },
+  {
+    "revision": "f8f0d54a15a751ee682dc16a70da953f",
+    "url": "/my-portfolio/static/media/unaweep.f8f0d54a.JPG"
+  }
+]);
